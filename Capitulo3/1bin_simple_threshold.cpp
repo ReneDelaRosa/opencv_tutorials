0edc9e6a9ba8ef5ce1ffe961519788955698da47
Rene De la Rosa <renedelarosapicazo@gmail.com>
2018-04-23 13:26:42 -0500

    Add files via upload

---
+++ Capitulo3/1bin_simple_threshold.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+	Mat input_image=imread("Lena.png",IMREAD_GRAYSCALE);
+	Mat binary_image(input_image.size(),input_image.type());
+	threshold(input_image,binary_image,128,255,THRESH_BINARY);
+	imwrite("THRESH_BINARY.png",binary_image);	
+	imshow("Imagen original",input_image);
+	imshow("Imagen binarizada",binary_image);
+	waitKey(0);
+	return 0;
+}
